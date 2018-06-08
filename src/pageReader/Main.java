69c1c5f33df3ec11f68d721a299c4ec5168aca7b
sophia-jihye <jihyeparkk@naver.com>
2018-06-08 13:00:19 +0900

    use mail api

--- src/pageReader/Main.java
+++ src/pageReader/Main.java
@@ -1,13 +1,31 @@
 package pageReader;
 
+import samples.mail.Gmail;
+
 public class Main {
 	public static void main(String[] args) {
 
 		Spider spider = new Spider();
 
+		String userId = "sophia.jihyeparkk";
+		String passwd = "111111";
+		String emailFrom = "sophia.jihyeparkk@gmail.com";
+		String emailTo = "jihyeparkk@naver.com";
+		String subject = "게시판 업데이트 감지~";
+		String content = "http://ie.snu.ac.kr/ko/board/8";
+
+		Gmail gmail = new Gmail();
+		Gmail.userId = userId;
+		Gmail.passwd = passwd;
+		gmail.setEmailFrom(emailFrom);
+		gmail.setEmailTo(emailTo);
+		gmail.setSubject(subject);
+		gmail.setContent(content);
+
 		while (true) {
 			boolean flag = spider.search("http://ie.snu.ac.kr/ko/board/8", "2018-06-08");
 			if (flag) {
+				gmail.sendMail();
 				break;
 			}
 			try {
